aac0edc68ed7d15d3d273978cc0a6d0467cbd549
FERNANDO PEREZ SANZ <fernando.perez8@um.es>
2020-12-28 11:53:03 +0100

    kk

---
+++ R/mynetplot.R
@@ -0,0 +1,59 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+mynetplot <- function(df, width = NULL, height = NULL, elementId = NULL) {
+
+  df2 <- df %>% mutate(genes = strsplit(genes, ",") )
+  df2$value = 10
+  df2$color = "#ff0000"
+  aux <- tibble( path=unique(unlist(df2$genes)), genes = NA, value = 3, color="#0000ff" )
+  df3 <- rbind(df2,aux) %>% select(path,value,genes,color) %>% as.data.frame()
+  names(df3) <- c("name","value","linkWith","color")
+  dfjson <- jsonlite::toJSON(df3)
+  # forward options using x
+  x = list(
+    data = dfjson
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'mynetplot',
+    x,
+    width = width,
+    height = height,
+    package = 'mynetplot',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for mynetplot
+#'
+#' Output and render functions for using mynetplot within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a mynetplot
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name mynetplot-shiny
+#'
+#' @export
+mynetplotOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'mynetplot', width, height, package = 'mynetplot')
+}
+
+#' @rdname mynetplot-shiny
+#' @export
+renderMynetplot <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, mynetplotOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/mynetplot.js
@@ -0,0 +1,77 @@
+HTMLWidgets.widget({
+
+  name: 'mynetplot',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        am4core.useTheme(am4themes_animated);
+// Themes end
+
+        var chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree);
+
+        var networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries());
+        networkSeries.dataFields.linkWith = "linkWith";
+        networkSeries.dataFields.name = "name";
+        networkSeries.dataFields.id = "name";
+        networkSeries.dataFields.value = "value";
+        networkSeries.dataFields.children = "children";
+        networkSeries.dataFields.color = "color";
+
+        networkSeries.nodes.template.label.text = "{name}";
+        networkSeries.fontSize = 12;
+        networkSeries.linkWithStrength = 0;
+
+        var nodeTemplate = networkSeries.nodes.template;
+        nodeTemplate.tooltipText = "{name}";
+        nodeTemplate.fillOpacity = 1;
+        nodeTemplate.label.hideOversized = true;
+        nodeTemplate.label.truncate = true;
+
+        var linkTemplate = networkSeries.links.template;
+        linkTemplate.strokeWidth = 1;
+        var linkHoverState = linkTemplate.states.create("hover");
+        linkHoverState.properties.strokeOpacity = 1;
+        linkHoverState.properties.strokeWidth = 2;
+
+        nodeTemplate.events.on("over", function (event) {
+            var dataItem = event.target.dataItem;
+            dataItem.childLinks.each(function (link) {
+                link.isHover = true;
+            });
+        });
+
+        nodeTemplate.events.on("out", function (event) {
+            var dataItem = event.target.dataItem;
+            dataItem.childLinks.each(function (link) {
+                link.isHover = false;
+            });
+        });
+
+        networkSeries.manyBodyStrength = -50;
+        networkSeries.links.template.distance = 1;
+        networkSeries.links.template.strength = 1;
+
+        networkSeries.data = x.data;
+
+      chart.exporting.menu = new am4core.ExportMenu();
+      chart.exporting.menu.align = "left";
+      chart.exporting.menu.verticalAlign = "top";
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
